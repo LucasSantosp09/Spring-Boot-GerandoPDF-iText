7a058c919c7e6b62d9c5a2c8ff2f01a824383797
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-07-16 22:19:51 -0300

    Gerando PDFs a partir de templates do Thymeleaf

--- src/main/java/Lucas/controllers/HomeController.java
+++ src/main/java/Lucas/controllers/HomeController.java
@@ -2,7 +2,11 @@ package Lucas.controllers;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties.Template;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
@@ -10,10 +14,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.itextpdf.html2pdf.HtmlConverter;
 import com.itextpdf.io.IOException;
 import com.itextpdf.io.source.ByteArrayOutputStream;
+
+import Lucas.models.Curso;
+
 
 
 
@@ -23,6 +32,10 @@ public class HomeController {
 	
 	@Autowired
 	private ResourceLoader resourceLoader;
+	
+	@Autowired
+	
+	private TemplateEngine templateEngine;
 
 	@GetMapping(produces = MediaType.APPLICATION_PDF_VALUE)
 	@ResponseBody
@@ -43,5 +56,24 @@ public class HomeController {
 		HtmlConverter.convertToPdf(htmlStream.getInputStream(), PdfStream); 
 		return PdfStream.toByteArray();
 							
+	}
+	
+	@GetMapping(value = "/cursos", produces = MediaType.APPLICATION_PDF_VALUE)
+	@ResponseBody
+	public byte[] gerarPdfTemplateThymeleaf() {
+		List<Curso> curso = new ArrayList<>();
+		curso.add(new Curso("Spring Framework - Fundamentos", "Cleysson Lima", 10));
+		curso.add(new Curso("Spring Framework - Template Thymeleaf", "Cleysson Lima", 10));
+		curso.add(new Curso("Spring Framework - Data JPA", "Cleysson Lima", 10));
+		
+		Context context = new Context();
+		context.setVariable("cursos", curso);
+		
+		String html  =  templateEngine.process("cursos.html", context);
+		
+		ByteArrayOutputStream pdfStream = new ByteArrayOutputStream();
+		HtmlConverter.convertToPdf(html, pdfStream);
+		
+		return pdfStream.toByteArray();
 	}
 }

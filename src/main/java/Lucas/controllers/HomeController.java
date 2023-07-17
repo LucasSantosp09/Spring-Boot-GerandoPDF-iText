ba6281110cc997a07c91b49f82fccac6d8ed957b
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-07-16 21:02:31 -0300

    Gerando PDF's a partir de um arquivo HTML

--- src/main/java/Lucas/controllers/HomeController.java
+++ src/main/java/Lucas/controllers/HomeController.java
@@ -1,5 +1,10 @@
 package Lucas.controllers;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.io.IOException;
 import com.itextpdf.io.source.ByteArrayOutputStream;
 
 
@@ -14,6 +20,9 @@ import com.itextpdf.io.source.ByteArrayOutputStream;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	@Autowired
+	private ResourceLoader resourceLoader;
 
 	@GetMapping(produces = MediaType.APPLICATION_PDF_VALUE)
 	@ResponseBody
@@ -22,5 +31,17 @@ public class HomeController {
 		ByteArrayOutputStream pdfSteam = new ByteArrayOutputStream();
 		HtmlConverter.convertToPdf("<h1 style ='color: red'>Olá, mundo ! </h1>",pdfSteam );
 		return pdfSteam.toByteArray();
+	}
+	
+	
+	@GetMapping(value = "/relatorio", produces = MediaType.APPLICATION_PDF_VALUE)
+	@ResponseBody
+	public byte[] gerarPdfArquivoHTML () throws java.io.IOException{
+		ByteArrayOutputStream PdfStream = new ByteArrayOutputStream();
+		//Resource htmlStream = resourceLoader.getResource("classpath:relatorio.html");
+		Resource htmlStream = resourceLoader.getResource("classpath:relatorio.html");
+		HtmlConverter.convertToPdf(htmlStream.getInputStream(), PdfStream); 
+		return PdfStream.toByteArray();
+							
 	}
 }
